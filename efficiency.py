b40568bf1749245c571fddc7378563b4c8aeddd1
taegwonbrew <jigun058@gmail.com>
2024-09-09 19:37:21 +0900

    1231312

--- efficiency.py
+++ efficiency.py
@@ -1,18 +1,16 @@
-def backtrack(path, options, result):
-    if not options:
-        result.append(path)
-        return
-    for i in range(len(options)):
-        # 다음 순열 생성을 위해 현재 숫자를 선택
-        backtrack(path + [options[i]], options[:i] + options[i+1:], result)
+# N 이하의 소수를 구하는 함수 
+def sieve_of_eratosthenes(N):
+    # N 크기의 리스트를 만들어서 모든 수를 소수(True)라고 가정
+    sieve = [True] * (N + 1)
+    sieve[0] = sieve[1] = False  # 0과 1은 소수가 아니니까 지운다
 
-def permute(nums):
-    result = []
-    backtrack([], nums, result)
-    return result
+    # !!중요!! 2부터 "루트N까지" 소수의 배수를 지움 -  간단한 최적화이니 해줍시다
+    for prime in range(2, int(N ** 0.5) + 1):
+        if sieve[prime]:  # prime이 소수인 경우
+            for multiple in range(prime * prime, N + 1, prime):
+                sieve[multiple] = False  # 'prime의 배수는 소수가 아닙니다' 라고 설정
 
-# 예시 실행: 1부터 N까지의 숫자에서 모든 순열 생성
-N = 3
-nums = list(range(1, N+1))
-all_permutations = permute(nums)
-print(all_permutations)
+    # 소수 반환하기
+    primes = [i for i, is_prime in enumerate(sieve) if is_prime]
+    return primes
+
