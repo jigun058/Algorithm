a0e4995a325006aa99c7284c4c2919acc8af3ddd
jigun058 <jigun058@gmail.com>
2024-09-25 18:50:04 +0900

    제발

--- 13702.py
+++ 13702.py
@@ -1,2 +1,32 @@
 N, K = map(int, input().split())
 
+cap = []
+for i in range(N):
+    cap.append(int(input()))
+    
+cap.sort()
+
+last = cap[0]
+mid = last // 2
+first = 0
+
+def test(A):
+    num = 0
+    for i in cap:
+        num += (i//A)
+    return num
+
+for _ in range(50):
+    if test(mid) < K:
+        last = mid
+        mid = (first + last) // 2
+    elif test(mid) > K:
+        first = mid
+        mid = (first + last) // 2
+    else:
+        while test(mid) == K:
+            
+            
+        break
+
+print(mid)
