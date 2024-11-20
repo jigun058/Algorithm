3abc99468fcc2a09ee72fd92dd7a5ef789726d9d
taegwonbrew <jigun058@gmail.com>
2024-11-20 16:51:37 +0900

    42423432

--- 11724.py
+++ 11724.py
@@ -2,7 +2,6 @@ from collections import deque
 
 V, E = map(int, input().split())
 
-ver = [i for i in range(1, V)]
 group = [0]*(V+1)
 adj = {i : [] for i in range(1, V+1)}
 
@@ -14,23 +13,19 @@ for i in range(E):
 queue = deque()
 gr = 0
 
-if V==1:
-    print(1)
+for i in range(1, V+1):
+    if group[i] != 0:
+        continue
 
-else:
-    while ver:
-        gr += 1
-        queue.append(ver[0])
+    gr += 1
+    queue.append(i)
+    group[i] = gr
+    while queue:
+        here = queue.popleft()
         
-        while queue:
-            here = queue.popleft()
-            if group[here] == 0:
-                group[here] = gr
-                if here in ver:
-                    ver.remove(here)
-            
-            for i in adj[here]:
-                if group[i] == 0:
-                    queue.append(i)
+        for j in adj[here]:
+            if group[j] == 0:
+                queue.append(j)
+                group[j] = gr
 
-    print(gr)
+print(gr)
